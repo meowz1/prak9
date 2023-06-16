e201252c2cdf10ca82285e10139cb813e3a992cc
Nikitos7777777777 <isip_n.s.savvatayev@mpt.ru>
2023-06-17 00:58:45 +0300

    first commit

---
+++ WpfEmal/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using ImapX.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfEmal
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (MailClick.SelectedItem as ComboBoxItem != null)
+            {
+                ImapHalper.Initialize((MailClick.SelectedItem as ComboBoxItem).Tag.ToString());
+                try
+                {
+
+                    if (ImapHalper.Login(Email.Text, Parol.Password))
+                    {
+                        bool open = false;
+                        Pochta windows = new Pochta();
+                        windows.ShowDialog();
+                    } 
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+
+            }
+            else
+            {
+                MessageBox.Show(Email.Text);
+            }
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            Email.Text = "syperkek777@mail.ru";
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
+//1639503416RchA
+//ginkmay@mail.ru
+//hfV4CKcrdvhjtGpe5bev
